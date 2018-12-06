4b0b4fb4e438d98f4d0df91519ca17997de4cefb
Farouklamri <farouk.lamrii@gmail.com>
2018-12-06 23:49:49 +0100

    map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MapComponent } from './components/map/map.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'map', component: MapComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
