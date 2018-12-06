e5439e4dc91af0a561e46614bbd1c61f02334569
esuau <evan.suau@etu.u-pec.fr>
2018-12-06 19:18:07 +0100

    feat(ui): Add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatBadgeModule,
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatListModule,
+  MatProgressBarModule,
+  MatSidenavModule,
+  MatToolbarModule, MatGridListModule, MatMenuModule
+} from '@angular/material';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressBarModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
